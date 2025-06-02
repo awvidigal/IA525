cf30a6563781708db6f261502e80604ec0082be8
awvidigal <allanwgvidigal@gmail.com>
2025-06-02 00:06:07 -0300

    versao de entrega

--- atividade1.py
+++ atividade1.py
@@ -23,7 +23,7 @@ def pointsPolygon(n):
     # Limite superior do raio
     radius = np.random.uniform(
         high= 0.5,
-        low= 0.01
+        low= 0.4
     )
 
     # Gera n ângulos aleatórios
@@ -97,10 +97,10 @@ def verificaPontos(points, polygon):
     # filtrar todos os pontos completamente a direita, a esquerda, acima e abaixo do poligono
     # identificar o vértice mais a direita e mais a esquerda (maior e menor x)
     # filtrar todos os pontos q estão mais a esquerda e mais a direita
-    # fazero mesmo para o eixo y
+    # fazer o mesmo para o eixo y
     # cerificar se o ponto está sobre uma reta do poligono
     # verificar no eixo x, para a direita, se uma reta a parir do ponto cruza o poligono um numero impar de vezes
-    # verificar no eixo y, para cima, se uma reta a partir do ponto cruza o poligono um numero impar de vezes
+    # ou verificar no eixo y, para cima, se uma reta a partir do ponto cruza o poligono um numero impar de vezes
     # garantir que nas verificações de cruzamento, a reta não cruze o poligono em um vértice
 
     # Empilha o primeiro ponto no final do vetor para fechar o polígono
@@ -120,10 +120,12 @@ def verificaPontos(points, polygon):
         retas[i] = [m,b]
     
     # Cria um vetor de pontos fora do polígono
-    pointsOut = np.zeros((len(points),2))
+    # pointsOut = np.zeros((len(points),2))
+    pointsOut = []
 
     # Cria um vetor de pontos dentro do polígono
-    pointsIn = np.zeros((len(points),2))
+    # pointsIn = np.zeros((len(points),2))
+    pointsIn = []
     
     # Pega o ponto mais a direita e mais a esquerda
     maxX = np.max(polygonPoints[:,0])
@@ -134,58 +136,91 @@ def verificaPontos(points, polygon):
     minY = np.min(polygonPoints[:,1])
     
     for point in range(len(points)):
+        # Verificar se o ponto está sobre um vértice do polígono. Se sim, está dentro
+        # Verificar se o ponto está sobre uma reta. Se sim, está dentro
+        # Verificar se ele está além dos pontos extremos do polígono, se sim, está fora
+        # Verificar quantas retas o ponto cruza para a direita. 
+            # Se for quantidade ímpar, está dentro
+            # Se for quantidade par, está fora
+
+        
         # Verifica se o ponto está completamente fora do polígono, observando os vértices mais extremos
         if points[point][0] > maxX or points[point][0] < minX or points[point][1] > maxY or points[point][1] < minY:
-            pointsOut[point] = points[point]
-            # points[point][0] = 0
-            # points[point][1] = 0
-            # points = np.delete(points, point, axis=0)
-            # Apagar os pontos no vetor de origem
-            # Reordenar para que não fiquem espaços vazios no vetor de origem
+            # pointsOut[point] = points[point]
+            pointsOut.append(points[point])
+            continue
 
-         
+        # Verifica se o ponto cruza um vertice
+        if any(np.isclose(points[point][0], vertex[0], atol=1e-6) for vertex in polygonPoints):
+            pointsOut.append(points[point])
+            continue
+        
         else:
-            # Verifica se o ponto está sobre uma aresta do polígono
+            # Verifica os cruzamentos em x                
+            contador = 0
             for reta in range(len(retas)):
-                # Verifica se o ponto está sobre a reta
-                if points[point][1] == (retas[reta][0] * points[point][0]) + retas[reta][1]:
-                    pointsIn[point] = points[point]
-                    # points[point][0] = 0
-                    # points[point][1] = 0
-                    # points = np.delete(points, point, axis=0)
-                    # Apagar os pontos no vetor de origem
-                    # Reordenar para que não fiquem espaços vazios no vetor de origem
-                    break
+                
+                # Verifica se existe a condição de uma reta vertical
+                if np.isclose(polygonPoints[reta][0], polygonPoints[reta+1][0], atol=1e-6):
+                    # Verifica se o ponto está sobre a reta vertical
+                    if np.isclose(points[point][0], polygonPoints[reta][0], atol=1e-6) and min(polygonPoints[reta][1], polygonPoints[reta + 1][1]) <= points[point][1] <= max(polygonPoints[reta][1], polygonPoints[reta + 1][1]):
+                        pointsIn.append(points[point])
+                        continue
+
+                    # Verifica se o ponto cruza a reta vertical
+                    if points[point][0] < polygonPoints[reta][0] and min(polygonPoints[reta][1], polygonPoints[reta + 1][1]) <= points[point][1] <= max(polygonPoints[reta][1], polygonPoints[reta + 1][1]):
+                        contador += 1
 
                 else:
-                    # Verifica os cruzamentos em x
-                    contador = 0
-                    for reta in range(len(retas)):
-                        # Verifica cruzamento em x
-                        cruzamentoX = (points[point][1] - retas[reta][1]) / retas[reta][0]
-                        if cruzamentoX > points[point][0]:
-                            contador += 1
-                    
-                    if contador % 2 == 1:
-                        pointsIn[point] = points[point]
-                        # points = np.delete(points, point, axis=0)
-                        # Apagar os pontos no vetor de origem
-                        # Reordenar para que não fiquem espaços vazios no vetor de origem
-                    # else:
-                    #     pointsOut[point] = points[point]
-                        # points = np.delete(points, point, axis=0)
-                        # Apagar os pontos no vetor de origem
-                        # Reordenar para que não fiquem espaços vazios no vetor de origem
+                    # Verifica se o ponto está sobre uma reta
+                    if np.isclose(points[point][1], (retas[reta][0] * points[point][0]) + retas[reta][1], atol=1e-6) and min(polygonPoints[reta][1], polygonPoints[reta + 1][1]) <= points[point][1] <= max(polygonPoints[reta][1], polygonPoints[reta + 1][1]):
+                        pointsIn.append(points[point])
+                        continue
 
-    inPoints = np.count_nonzero(
-        a= pointsIn,
-        axis= 0
-    )
+                    # Verifica cruzamento em x
+                    cruzamentoX = (points[point][1] - retas[reta][1]) / retas[reta][0]
+                    if cruzamentoX > points[point][0] and min(polygonPoints[reta][1], polygonPoints[reta + 1][1]) <= points[point][1] <= max(polygonPoints[reta][1], polygonPoints[reta + 1][1]):
+                        contador += 1
+
+            if contador % 2 == 1:
+                pointsIn.append(points[point])
+            else:
+                pointsOut.append(points[point])
+ 
+
+    inPoints = len(pointsIn)/len(points)
     
-    return inPoints[0]
+    return inPoints
+
+def verificaArea(polygon):
+    '''
+        Verifica a área do polígono
+
+        Parâmetros:
+        -----------
+        polygon : vetor
+            Lista de pontos do poligono
+
+        Retorno:
+        --------
+        area : float
+            Área do polígono
+    '''
+    # Empilha o primeiro ponto no final do vetor para fechar o polígono
+    polygonPoints = np.vstack([polygon, polygon[0]])
+
+    # Calcula a área do polígono usando a fórmula de Shoelace
+    areaVerificada = 0.5 * np.abs(np.dot(polygonPoints[:, 0], np.roll(polygonPoints[:, 1], 1)) - np.dot(np.roll(polygonPoints[:, 0], 1), polygonPoints[:, 1]))
+
+    return areaVerificada
                    
 
-polygon = pointsPolygon(3)
+polygon = pointsPolygon(12)
 aleatory = plotPolygon(polygon,10000)
-area = verificaPontos(aleatory, polygon)/len(aleatory)
-print(f"Área do polígono: {area}")
+area = verificaPontos(aleatory, polygon)
+confirmaArea = verificaArea(polygon)
+
+print(f"Área do polígono calculada: {area}")
+print(f"Área do polígono verificada: {confirmaArea}")
+erro = np.abs(confirmaArea - area) / confirmaArea * 100
+print(f"Erro: {erro:.2f}%")
