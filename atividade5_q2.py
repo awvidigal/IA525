6c8a6d2e04e9e48e021b7a9fec1dccfbbd3a37f1
awvidigal <allanwgvidigal@gmail.com>
2025-07-06 01:34:59 -0300

    atividade 5: questao 2 concluida de verdade

--- atividade5_q2.py
+++ atividade5_q2.py
@@ -98,23 +98,88 @@ def identificadorQuadradosMinimos(t, y, x_test):
 
     return yPredTeste, yPredTreino
 
-def encontraCoeficientesSVM(t, y, especie):
-    yBinario = np.where(y == especies[especie], 1, -1)
+def identificadorSVM(t, y, x_test):
+    yBinSetosa      = np.where(y == especies['setosa'], 1, -1)
+    yBinVersicolor  = np.where(y == especies['versicolor'], 1, -1)
+    yBinVirginica   = np.where(y == especies['virginica'], 1, -1)
     
     m = t.shape[1]
-    
-    t = t.T
+    x_train = t
 
+    t = t.T    
+    
+
+    # Identificar setosas
     a = Variable(m)
     b = Variable()
 
-    funcao = pos(1 - multiply(yBinario, (a.T @ t + b)))
+    funcao = pos(1 - multiply(yBinSetosa, (a.T @ t + b)))
     objective = Minimize(sum(funcao))
     problema = Problem(objective)
-
     problema.solve()
 
-    return a.value, b.value
+    a = a.value
+    a.reshape(-1,1)
+
+    yPredSetosaTeste = np.sign(x_test @ a + b.value)
+    yPredSetosaTeste = np.where(yPredSetosaTeste == 1, especies['setosa'], -1)
+
+    yPredSetosaTreino = np.sign(x_train @ a + b.value)
+    yPredSetosaTreino = np.where(yPredSetosaTreino == 1, especies['setosa'], -1)
+
+     # Identificar versicolor
+    a = Variable(m)
+    b = Variable()
+
+    funcao      = pos(1 - multiply(yBinVersicolor, (a.T @ t + b)))
+    objective   = Minimize(sum(funcao))
+    problema    = Problem(objective)
+    problema.solve()
+
+    a = a.value
+    a.reshape(-1,1)
+    
+    yPredVersicolorTeste    = np.sign(x_test @ a + b.value)
+    yPredVersicolorTreino   = np.sign(x_train @ a + b.value)
+
+    # Identificar virginica
+    a = Variable(m)
+    b = Variable()
+
+    funcao      = pos(1 - multiply(yBinVirginica, (a.T @ t + b)))
+    objective   = Minimize(sum(funcao))
+    problema    = Problem(objective)
+    problema.solve()
+
+    a = a.value
+    a.reshape(-1,1)    
+    
+    yPredVirginicaTeste = np.sign(x_test @ a + b.value)
+    yPredVirginicaTeste = np.where(yPredVirginicaTeste == 1, especies['virginica'], -1)
+
+    yPredVirginicaTreino = np.sign(x_train @ a + b.value)
+    yPredVirginicaTreino = np.where(yPredVirginicaTreino == 1, especies['virginica'], -1)
+    
+    yPredTeste  = []
+    yPredTreino = []
+
+    for item in range(len(yPredSetosaTeste)):
+        yPredTeste.append(max(yPredSetosaTeste[item], yPredVersicolorTeste[item], yPredVirginicaTeste[item]))
+        if yPredTeste[item] == -1:
+            if y[item] > 0:
+                yPredTeste[item] = 0
+            else:
+                yPredTeste[item] = 2
+
+    for item in range(len(yPredSetosaTreino)):
+        yPredTreino.append(max(yPredSetosaTreino[item], yPredVersicolorTreino[item], yPredVirginicaTreino[item]))
+        if yPredTreino[item] == -1:
+            if y[item] > 0:
+                yPredTreino[item] = 0
+            else:
+                yPredTreino[item] = 2
+
+    return yPredTeste, yPredTreino
 
 # print(type(iris))
 x = iris.data
@@ -138,6 +203,9 @@ yTrainSetosa     = np.where(yTrain == especies['setosa'], 1, -1)
 yTrainVirginica  = np.where(yTrain == especies['virginica'], 1, -1)
 yTrainVersicolor = np.where(yTrain == especies['versicolor'], 1, -1)
 
+######################################################################################
+#                                   INICIO ID QM
+######################################################################################
 y_predTeste, y_predTreinamento = identificadorQuadradosMinimos(xTrain, yTrain, xTest)
 
 y_predTeste         = [int(val) for val in y_predTeste]
@@ -162,10 +230,49 @@ print('\nMatriz de Confusão QM -> Variáveis de teste:')
 print(matrizConfusaoTeste)
 
 elementosTeste = matrizConfusaoTeste.sum()
-print('Soma dos elementos da matriz de teste: ', elementosTeste)
+print('Soma dos elementos da matriz de teste QM: ', elementosTeste)
 
 print('\nMatriz de Confusão QM -> Variáveis de treinamento:')
 print(matrizConfusaoTreinamento)
 
 elementosTreinamento = matrizConfusaoTreinamento.sum()
-print('Soma dos elementos da matriz de treinamento: ', elementosTreinamento)
+print('Soma dos elementos da matriz de treinamento QM: ', elementosTreinamento)
+
+######################################################################################
+#                                   FINAL ID QM
+######################################################################################
+
+######################################################################################
+#                                  INICIO ID SVM
+######################################################################################
+y_predTeste, y_predTreinamento = identificadorSVM(xTrain, yTrain, xTest)
+
+y_predTeste         = [int(val) for val in y_predTeste]
+y_predTreinamento   = [int(val) for val in y_predTreinamento]
+
+# print(y_predTeste)
+# print(y_predTreinamento)
+
+matrizConfusaoTeste = confusion_matrix(
+    y_true= yTest,
+    y_pred= y_predTeste,
+    labels= classesValidas
+)
+
+matrizConfusaoTreinamento = confusion_matrix(
+    y_true= yTrain,
+    y_pred= y_predTreinamento,
+    labels= classesValidas
+)
+
+print('\nMatriz de Confusão SVM -> Variáveis de teste:')
+print(matrizConfusaoTeste)
+
+elementosTeste = matrizConfusaoTeste.sum()
+print('Soma dos elementos da matriz de teste SVM: ', elementosTeste)
+
+print('\nMatriz de Confusão SVM -> Variáveis de treinamento:')
+print(matrizConfusaoTreinamento)
+
+elementosTreinamento = matrizConfusaoTreinamento.sum()
+print('Soma dos elementos da matriz de treinamento SVM: ', elementosTreinamento)
